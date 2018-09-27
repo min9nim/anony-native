5c5fc1e36d35b85fc50f9da40da88a3dc087cf0f
songmingu(mac) <min9nim@gmail.com>
2018-09-27 17:45:35 +0900

    마크다운 추가

--- pages/Post.js
+++ pages/Post.js
@@ -2,6 +2,11 @@ import React from "react";
 import { StyleSheet, View, ScrollView, ActivityIndicator } from 'react-native';
 import {Button, Text} from 'native-base';
 import { Font } from "expo";
+//import Markdown from 'react-native-simple-markdown';
+//import Markdown from 'react-native-markdown-renderer';
+import { MarkdownView } from 'react-native-markdown-view';
+
+
 
 //import {Excerpt, Menu, Search} from "../components";
 import {tp} from "../com/app.js";
@@ -19,7 +24,8 @@ export default class Post extends React.Component {
     async componentWillMount() {
         await Font.loadAsync({
             Roboto: require("native-base/Fonts/Roboto.ttf"),
-            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+            Courier : require("native-base/Fonts/Courier.ttf")
         });
         this.setState({ loading: false });
     }    
@@ -37,11 +43,17 @@ export default class Post extends React.Component {
         }        
 
         return (
-            <View style={styles.container}>
+        <View style={styles.container}>
             <ScrollView style={styles.scrollView} contentContainerStyle={styles.contentContainer}>
                 <Text style={styles.title}>{this.props.post.title}</Text>
                 <Text style={styles.writer}>{this.props.post.writer} - {Date(this.props.post.date).substr(0,21)}</Text>
-                <Text style={styles.content}>{this.props.post.content}</Text>
+                {
+                    this.props.post.isMarkdown
+                    ?
+                    <MarkdownView>{this.props.post.content}</MarkdownView>
+                    :
+                    <Text style={styles.content}>{this.props.post.content}</Text>
+                }
                 <View style={styles.btnWrapper}>
                     <Button success small style={styles.btn} onPress={this.goList}><Text> List </Text></Button><Button success small style={styles.btn}><Text> Write </Text></Button>
                 </View>
@@ -96,6 +108,21 @@ const styles = StyleSheet.create({
         marginRight: 5,
     }
 
-
   });
   
+
+  const markdownStyles = {
+    heading1: {
+      fontSize: 24,
+      color: 'purple',
+    },
+    link: {
+      color: 'pink',
+    },
+    mailTo: {
+      color: 'orange',
+    },
+    text: {
+      color: '#555555',
+    },
+  }
